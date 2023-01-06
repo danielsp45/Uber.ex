be2eab8a9f815166e1a86c27ccaf4ffdac1e2d5f
daniel_sp <danielsp356@gmail.com>
2023-01-06 01:19:42 +0000

    Restart parser

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,3 +1,16 @@
 defmodule Uber.Parser do
 
+  def parse_drivers(filename) do
+    filename
+    |> Path.expand(__DIR__)
+    |> File.stream!
+    |> CSV.decode!
+    |> Enum.take_every(1)
+  end
+
+  # def parse_users(users_csv) do
+  # end
+
+  # def parse_rides(rides_csv) do
+  # end
 end
